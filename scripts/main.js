0c6bb1b5f055580ddaa5f2999d00160b5b3f6ccf
kvatoferma <jimmaggot@mail.ru>
2018-01-24 00:22:37 +0300

    Change content from Eisenstein to Github, add JS greeting and change-user button

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,23 @@ myImage.onclick = function(){
 		myImage.setAttribute ('src','images/my-image.png');
 	}
 }
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+	var myName = prompt ('Please enter your name.');
+	localStorage.setItem ('name',myName);
+	myHeading.textContent = 'Github is cool, '+ myName + '!';
+}
+
+if (!localStorage.getItem('name')) {
+	setUserName();
+} else{
+	var storedName = localStorage.getItem('name');
+	myHeading.textContent = 'Github is cool, '+ storedName + '!';
+}
+
+myButton.onclick = function (){
+	setUserName();
+}
